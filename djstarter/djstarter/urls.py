f79b7491cdbf3d244f3d324d51a31d59d3a36340
Kangmin Xie <kangmin.xie@gmail.com>
2019-10-16 23:36:32 -0500

    DJAG-0002 added a demo view and tried url vs path urls.py

--- djstarter/djstarter/urls.py
+++ djstarter/djstarter/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+# from django.conf.urls import url
 from django.urls import path
+from demo_app import views
 
 urlpatterns = [
+    # url(r'^$', views.demo_index, name='demo-index'),  # if use old style
+    path('', views.demo_index, name='demo-index'),
     path('admin/', admin.site.urls),
 ]
